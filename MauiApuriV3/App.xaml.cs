85cb80a611bcf8d5a6d0cce10aa78d9c785ba271
areee <10089872+areee@users.noreply.github.com>
2022-06-23 11:19:46 +0300

    Add Maui Blazor project & use Component1 from ApuriV3ClassLibrary

---
+++ MauiApuriV3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MauiApuriV3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
